ff6699680a5d433dd9b876393a729a655c40af59
s0m35h1t <adib.grouz@gmail.com>
2019-12-26 18:37:26 +0000

    [update] window size

--- grid.c
+++ grid.c
@@ -116,7 +116,7 @@ void draw_grid(SDL_Instance instance, float **z)
 	extern unsigned int size;
 	int res = size - 1;
 	float inclination = 0.7;
-	int width = WIN_WIDTH * 0.8;
+	int width = WIN_WIDTH * 0.6;
 	int height = WIN_HEIGHT  * 0.8;
 	int x = 0, y = 0, dx = (width / res) - 1, dy = (height / res) - 1;
 	int X = 0, Y = 0, Wx = 0, Wy = 0, xOffset = 270, yOffset = 150;
